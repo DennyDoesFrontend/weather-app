34fdb89497f704dd803dffab8384b7640dc662a8
Denzel Ashitey <denzelashiteyfd@gmail.com>
2024-06-14 01:06:49 +0000

    UI Revamp && map removal

---
+++ script.js
@@ -0,0 +1,48 @@
+const degree = document.querySelector(".degree");
+const getLocation = document.querySelector(".myLocation");
+const time = document.querySelector(".time");
+const icon = document.querySelector(".dataIcon");
+const situation = document.querySelector(".situation");
+const precipitation = document.querySelector(".precipitation");
+const humidity = document.querySelector(".humidity");
+const temperature = document.querySelector(".temperature");
+const cloud = document.querySelector(".cloud");
+const inputValue = document.querySelector(".input-value");
+const btn = document.querySelector(".button");
+const apiKey = "9cd9ef8afd014c7f99f223758232912";
+let myLocation = inputValue.value;
+let documentBody = document.querySelector("body");
+const results = document.querySelector(".results");
+
+async function fetchData() {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${myLocation}`
+    );
+    if (!response.ok) {
+      throw new Error("Problem Fetching Data");
+    }
+    const data = await response.json();
+    degree.textContent = data.current.temp_c + "°";
+    getLocation.textContent = data.location.name;
+    time.textContent = data.current.last_updated;
+    icon.src = data.current.condition.icon;
+    situation.textContent = data.current.condition.text;
+    precipitation.textContent = data.current.precip_mm + "mm";
+    humidity.textContent = data.current.humidity;
+    temperature.textContent = data.current.temp_c + "°";
+    cloud.textContent = data.current.cloud;
+  } catch (error) {
+    alert(error);
+  }
+}
+
+btn.addEventListener("click", () => {
+  if (myLocation) {
+    myLocation = inputValue.value;
+    results.style.display = "flex";
+    fetchData();
+  } else {
+    alert("Please input a location to search");
+  }
+});
